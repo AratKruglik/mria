485b103419b1e16c11c599ac6d9bf38b9ff9a942
Arat <alexkruglik81@gmail.com>
2015-08-04 22:53:36 +0300

    database settings

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'mria',
+			'username'  => 'mria',
+			'password'  => '1',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
